10d3db6350eaef886af99869628ccceb872065b6
Kate Mitchell <mail@katemitchell.org.uk>
2018-10-09 21:09:12 +0100

    Add disclaimer below image gallery if product is soap product. Define new property on Product model. Format disclaimer with css.

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,1 +1,13 @@
-from oscar.apps.catalogue.models import *  # noqa isort:skip
+from oscar.apps.catalogue.abstract_models import AbstractProduct
+
+
+class Product(AbstractProduct):
+
+    # TODO: Test this
+    @property
+    def is_soap_product(self):
+        product_categories = set(category.name for category in self.categories.all())
+        soap_categories = {'Soap with Bag', 'Soap Refills'}
+        return product_categories.intersection(soap_categories)
+
+from oscar.apps.catalogue.models import *
