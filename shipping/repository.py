7aa4c1274defda6ca6c453ba5a1c2b331b76b6e8
Kate Mitchell <mail@katemitchell.org.uk>
2019-03-09 22:01:47 +0700

    Implement free UK shipping on orders over 5 GBP

--- shipping/repository.py
+++ shipping/repository.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from oscar.apps.shipping import repository
 
 from shipping import methods
@@ -6,7 +7,9 @@ from shipping import methods
 class Repository(repository.Repository):
 
     def get_available_shipping_methods(self, basket, user=None, shipping_addr=None, request=None, **kwargs):
-        shipping_methods = (methods.FreeRoyalMailFirstClass(), )
+        shipping_methods = ((methods.RoyalMailLargeLetterFirstClass(), )
+                            if basket.total_incl_tax <= Decimal('5.00')
+                            else (methods.FreeRoyalMailFirstClass(), ))
         if shipping_addr and shipping_addr.country.code == 'ES':
             shipping_methods = (methods.RoyalMailInternationalStandard(), )
         return shipping_methods
--- tests/shipping/test_repository.py
+++ tests/shipping/test_repository.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.test import TestCase
 from mock import Mock
 
@@ -9,15 +10,23 @@ class RepositoryTest(TestCase):
 
     def setUp(self):
         self.repository = Repository()
+        self.mock_basket = Mock()
+        self.mock_basket.total_incl_tax = Decimal('5.99')
 
-    def test_get_available_shipping_methods_returns_free_royal_mail_first_class_for_uk(self):
-        shipping_methods = self.repository.get_available_shipping_methods(Mock())
+    def test_get_available_shipping_methods_returns_royal_mail_large_letter_first_class_if_basket_lte_5(self):
+        self.mock_basket.total_incl_tax = Decimal('5.00')
+        shipping_methods = self.repository.get_available_shipping_methods(self.mock_basket)
+        self.assertIn(methods.RoyalMailLargeLetterFirstClass.code,
+                      (shipping_method.code for shipping_method in shipping_methods))
+
+    def test_get_available_shipping_methods_returns_free_royal_mail_first_class_if_basket_gt_5(self):
+        shipping_methods = self.repository.get_available_shipping_methods(self.mock_basket)
         self.assertIn(methods.FreeRoyalMailFirstClass.code,
                       (shipping_method.code for shipping_method in shipping_methods))
 
     def test_get_available_shipping_methods_returns_royal_mail_intl_std_for_spain(self):
         address = Mock()
         address.country.code = 'ES'
-        shipping_methods = self.repository.get_available_shipping_methods(Mock(), shipping_addr=address)
+        shipping_methods = self.repository.get_available_shipping_methods(self.mock_basket, shipping_addr=address)
         self.assertIn(methods.RoyalMailInternationalStandard.code,
                       (shipping_method.code for shipping_method in shipping_methods))
