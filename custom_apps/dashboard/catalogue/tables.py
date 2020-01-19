8639026f04a5c3dfad27db8f22524a8be37e814b
Kate Mitchell <mail@katemitchell.org.uk>
2020-01-19 12:56:22 -0500

    Fix template paths in dashboard ProductTable following v2 upgrade. Added wip.md file to document any work in progress.

--- custom_apps/dashboard/catalogue/tables.py
+++ custom_apps/dashboard/catalogue/tables.py
@@ -11,11 +11,11 @@ Product = get_model('catalogue', 'Product')
 class ProductTable(tables.ProductTable):
     title = TemplateColumn(
         verbose_name=_('Title'),
-        template_name='dashboard/catalogue/product_row_title.html',
+        template_name='oscar/dashboard/catalogue/product_row_title.html',
         order_by='title', accessor=A('title'))
     image = TemplateColumn(
         verbose_name=_('Image'),
-        template_name='dashboard/catalogue/product_row_image.html',
+        template_name='oscar/dashboard/catalogue/product_row_image.html',
         orderable=False)
     product_class = Column(
         verbose_name=_('Product type'),
@@ -23,24 +23,24 @@ class ProductTable(tables.ProductTable):
         order_by='product_class__name',)
     variants = TemplateColumn(
         verbose_name=_("Variants"),
-        template_name='dashboard/catalogue/product_row_variants.html',
+        template_name='oscar/dashboard/catalogue/product_row_variants.html',
         orderable=False
     )
     stock_records = TemplateColumn(
         verbose_name=_('Stock records'),
-        template_name='dashboard/catalogue/product_row_stockrecords.html',
+        template_name='oscar/dashboard/catalogue/product_row_stockrecords.html',
         orderable=False)
     num_in_stock = TemplateColumn(
         verbose_name=_('Num in stock'),
-        template_name='dashboard/catalogue/product_row_numinstock.html',
+        template_name='oscar/dashboard/catalogue/product_row_numinstock.html',
         orderable=False)
     num_allocated = TemplateColumn(
         verbose_name=_('Num allocated'),
-        template_name='dashboard/catalogue/product_row_numallocated.html',
+        template_name='oscar/dashboard/catalogue/product_row_numallocated.html',
         orderable=False)
     actions = TemplateColumn(
         verbose_name=_('Actions'),
-        template_name='dashboard/catalogue/product_row_actions.html',
+        template_name='oscar/dashboard/catalogue/product_row_actions.html',
         orderable=False)
 
     icon = "sitemap"
---
+++ wip.md
@@ -0,0 +1,8 @@
+Custom dashboard templates not working, for product:
+
+```
+TypeError at /dashboard/catalogue/
+'NoneType' object is not subscriptable
+```
+
+It seems to be something to do with the product image... needs further investigation.
