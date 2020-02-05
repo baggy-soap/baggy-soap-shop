3a4b5b558395100a709bc9d667b16c7888b9f627
Kate Mitchell <mail@katemitchell.org.uk>
2020-02-04 20:09:18 -0500

    Add admain email handler to LOGGING

--- baggysoapshop/settings.py
+++ baggysoapshop/settings.py
@@ -212,6 +212,11 @@ LOGGING = {
             'filename': 'baggysoap.log',
             'formatter': 'verbose'
         },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        },
     },
     'loggers': {
         'django': {
