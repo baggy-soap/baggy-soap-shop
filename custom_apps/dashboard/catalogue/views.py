8b629489d2f09742fd4a2cddfb32885f67fc130a
Kate Mitchell <mail@katemitchell.org.uk>
2020-02-17 21:58:22 -0500

    Remove browsable_dashboard override and just use entire product queryset.

--- custom_apps/dashboard/catalogue/views.py
+++ custom_apps/dashboard/catalogue/views.py
@@ -4,13 +4,13 @@ from oscar.apps.dashboard.catalogue import views
 
 class ProductListView(views.ProductListView):
 
-    # TODO: Test
     def get_queryset(self):
         """
         Build the queryset for this list, returning as a list to prevent tables2 from passing
         ordering to the database (we want a Python list sort using the appropriate key)
         """
-        queryset = views.Product.objects.browsable_dashboard().base_queryset()
+        # Get all products, including children (i.e. don't use browsable_dashboard queryset)
+        queryset = views.Product.objects.base_queryset()
         queryset = self.filter_queryset(queryset)
         queryset = self.apply_search(queryset)
         return list(queryset)
--- wip.md
+++
@@ -1,6 +0,0 @@
-
-
-### TODO
-
-* unit test for get_queryset
-* perhaps remove call to browsable_dashboard, and override of default?
